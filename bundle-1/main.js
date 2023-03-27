8a711eba7e762025a19b0715a433daa8070b5b5a
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-03-27 16:31:38 +0200

    Add: bundle 1

--- bundle-1/main.js
+++ bundle-1/main.js
@@ -9,7 +9,7 @@
 
 *******************************************************************************/
 
-
+/*
 // ESERCIZIO 1
 function checkAge(myAge) {
     let message = '';
@@ -22,27 +22,41 @@ function checkAge(myAge) {
     return message;
 }
 console.log(checkAge(17));
-// Eliminata la costante myAge all'interno della funzione in quanto myAge dev'essere il parametro della funzione stessa
-// Modificata variabile message da const a let
-// Inserito return message nella funzione
+// 1- Inserita una età verifica se è minore o maggiore di 18
+// 2- Eliminata la costante myAge all'interno della funzione in quanto myAge dev'essere il parametro della funzione stessa
+//    Modificata variabile message da const a let
+// 3- Inserito return message nella funzione
+*/
 
 /*
 // ESERCIZIO 2
 function printColorsNumber() {
     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
-    console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
+    console.log(`Nella mia palette ci sono ${colors.length} colori!`);
 }
 printColorsNumber();
+// 1- Stampa il numero di colori totale che sono presenti nell'array
+// 2- Sì è stato scritto .lenght al posto di .length
+// 3- No
 
+*/
 
+/*
 // ESERCIZIO 3
 function addNumbers() {
-    const userNumber = prompt('Inserisci un numero');
+    const userNumber = parseInt(prompt('Inserisci un numero'));
     const total = userNumber + 12;
 
     console.log(`Il risultato finale è ${total}`);
 }
 addNumbers();
+
+// 1- Dato un numero di ingresso viene sommato a 12
+// 2- No
+// 3- Sì, bisogna passare il numero inserito da stringa a numero con parseInt
+
+*/
+
 
 
 // ESERCIZIO 4
@@ -55,10 +69,10 @@ function checkAccess() {
     ];
     const userEmail = prompt('Inserisci il tuo indirizzo email');
 
-    let grantAccess = 'false';
+    let grantAccess = false;
 
     if (addresses.includes(userEmail)) {
-        grantAccess = 'true';
+        grantAccess = true;
     }
 
     if (grantAccess === true) {
@@ -68,4 +82,8 @@ function checkAccess() {
     }
 }
 checkAccess();
-*/
+
+// 1- Inserito un indirizzo mail verifica se è presente nell'array, se è presente viene visualizzato il messaggio accesso consentito altrimenti
+//      accesso negato
+// 2- Sì, i valori booleani erano stati definiti come stringhe
+// 3- No
